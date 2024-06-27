6baf3086e063456fdb5538616c76e827920e1207
monyjiji <157226401+monyjiji@users.noreply.github.com>
2024-06-27 19:25:19 +0200

    Add files via upload

---
+++ src/pages/Patients/PatNot.jsx
@@ -0,0 +1,182 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../css/patnot.css';
+
+
+
+    function PatNot() {
+    return( 
+      
+      
+     
+
+
+
+
+      <div className="container-fluid full-height-container">
+      
+    <div className="row w-10" style={{marginTop:'15px',marginBottom:'15px'}}>
+      <div className="col-12 text-center" style={{borderRadius:'10px'}}>
+
+    <div className="custom-shadow rounded ">
+        <div className="box-title border-bottom p-3">
+          <h6 className="m-0" style={{textAlign:'left'}}>Recent</h6>
+        </div>
+        <div className="border-bottom p-2" >
+              <div className="border-bottom">
+              <div className="font-weight-bold mr-3 p-1" style={{textAlign:'left'}}>
+             
+                <div className=" d-flex justify-content-between ">
+        <div className="text-truncate" style={{fontSize:20}}>DAILY RUNDOWN: SATURDAY</div>
+        <span class="d-block text-right mt-2">
+                           
+                                 <button className="btn btn-secondary" >Delete</button>
+                                 
+         </span>
+         </div>
+        <div className="small" >Pellentesque semper ex diam, at tristique ipsum varius sed. Pellentesque non metus ullamcorper</div>
+      </div>
+     
+     
+      
+      
+      
+    </div>
+
+
+    <div className="box-body " >
+              <div className="border-bottom">
+              <div className="font-weight-bold mr-3 p-1" style={{textAlign:'left'}}>
+              <div className=" d-flex justify-content-between ">
+        <div className="text-truncate" style={{fontSize:20}}>DAILY RUNDOWN: SATURDAY</div>
+        <span class="d-block text-right mt-2">
+                           
+                                 <button className="btn btn-secondary" >Delete</button>
+                                 
+         </span>
+         </div>
+
+
+        <div className="small">Pellentesque semper ex diam, at tristique ipsum varius sed. Pellentesque non metus ullamcorper</div>
+      </div>
+     
+      
+    
+      
+    </div>
+          </div>
+          
+           </div>   
+           </div>
+
+           
+
+
+
+
+
+  
+
+          </div>
+
+
+          </div>
+          <div className="row w-10" style={{marginTop:'15px',marginBottom:'15px'}}>
+          <div className="col-12 text-center" style={{borderRadius:'10px'}}>
+
+
+          <div className="custom-shadow rounded ">
+<div className="box-title border-bottom p-3">
+  <h6 className="m-0" style={{textAlign:'left'}}>Earlier</h6>
+  </div>
+  <div className="box-body border-bottom p-2" >
+              <div className="border-bottom">
+              <div className="font-weight-bold mr-3 p-1" style={{textAlign:'left'}}>
+              <div className=" d-flex justify-content-between ">
+        <div className="text-truncate" style={{fontSize:20}}>DAILY RUNDOWN: SATURDAY</div>
+        <span class="d-block text-right mt-2">
+                           
+                           <button className="btn btn-secondary" >Delete</button>
+                           
+   </span>
+   </div>
+
+
+
+        <div className="small">Pellentesque semper ex diam, at tristique ipsum varius sed. Pellentesque non metus ullamcorper</div>
+      </div>
+     
+     
+     
+      
+      
+    </div>
+
+
+    <div className="box-body " >
+              <div className="border-bottom">
+              <div className="font-weight-bold mr-3 p-1" style={{textAlign:'left'}}>
+              <div className=" d-flex justify-content-between ">
+        <div className="text-truncate" style={{fontSize:20}}>DAILY RUNDOWN: SATURDAY</div>
+        <span class="d-block text-right mt-2">
+                           
+                           <button className="btn btn-secondary" >Delete</button>
+                           
+   </span>
+   </div>
+        <div className="small">Pellentesque semper ex diam, at tristique ipsum varius sed. Pellentesque non metus ullamcorper</div>
+      </div>
+     
+     
+      
+      
+    </div>
+          </div>
+          
+           </div>   
+           </div>
+
+           </div>
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+ 
+
+ 
+
+          
+
+
+
+
+
+
+
+          
+
+
+
+
+
+          
+
+
+
+
+            );
+        }
+        
+             
+           
+          
+          export default PatNot;
